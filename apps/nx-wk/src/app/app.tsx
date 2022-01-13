4a3866540dfef7adaa548436e561072083e26fbe
nicolibeo <nicolas.labbe@libeo.io>
2022-01-13 08:54:13 +0100

    initial commit

---
+++ apps/nx-wk/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="nx-wk" />
+    </>
+  );
+}
+
+export default App;
